d3d9bef3dc9099c3fc89c5ca5f179989492df804
mohamedagwa2004 <120852466+mohamedagwa2004@users.noreply.github.com>
2022-12-18 01:49:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+int Startnumber, Endnumber;
+Console.WriteLine("Please Write Perfect Number");
+
+Console.WriteLine("______________________________________________________________________________");
+
+Console.Write("Please Enter Start Number:-");
+Startnumber = int.Parse(Console.ReadLine());
+
+Console.Write("Please Enter End Number:-");
+Endnumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Perfect Number" +Startnumber + "&" +Endnumber);
+
+for (int i = Startnumber; i<= Endnumber; i++)
+{
+    int sum = 0;
+    for (int x = 1; x<i; x++)
+    {
+        if (i % x ==0)
+            sum = sum + x;
+    }
+    if (sum == i)
+        Console.WriteLine("\t" + i);
+}
+
+
